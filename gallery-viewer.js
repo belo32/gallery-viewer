5d0d2512e2df65e59e0851c7af20838d2fb35aaf
Bilal Al <bilal@server.com>
2012-11-21 01:16:02 -0500

    first commit to gh-pages

---
+++ gallery-viewer.js
@@ -0,0 +1,17 @@
+/**
+* 
+*/
+var albumUrl = "http://api.imgur.com/2/album/UcUlZ.json"
+$(document).ready(function(){
+	$.getJSON(albumUrl,
+		function(data){
+			var images = data.album.images;
+			$.each(images, function(i,item){
+				var div = $("<div></div>").attr("id","gallery-item");
+				$("<img/>").attr("src",item.links.large_thumbnail).appendTo(div);
+				div.appendTo("#dynamic-gallery");
+			});			
+		});
+});
+
+
