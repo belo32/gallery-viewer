c1e281e710325ad4124e5a7bba0e865a4e3e56d9
Bilal Al <bilal@server.com>
2012-11-25 10:56:15 -0500

    commit changes to viewer

--- README.md
+++
@@ -1,6 +0,0 @@
-gallery-viewer
-==============
-belo32.github.com/gallery-viewer
-A script to display images dynamically on gh-pages static hosting from imgur.com api
-
-
--- gallery-viewer.js
+++ gallery-viewer.js
@@ -1,18 +1,22 @@
 /**
 * 
 */
-var id = "UcUlZ";
+var id = "qOlBw"; /* the album id of the imgur*/
 var albumUrl = "http://api.imgur.com/2/album/"+id+".json";
-$(document).ready(function(){
+$(document).ready(function(){ 
 	$.getJSON(albumUrl,
 		function(data){
 			var images = data.album.images;
 			$.each(images, function(i,item){
-				var div = $("<div></div>").attr("id","gallery-item");
+				var div = $("<div></div>").attr({
+					id: "gallery-item",
+					class: "grid_4"
+			});
 				$("<img/>").attr("src",item.links.large_thumbnail).appendTo(div);
 				div.appendTo("#dynamic-gallery");
 			});			
 		});
+
 });
 
 
