8980fab40104cde5da0fb53cfa5a8de09752e680
Bilal Al <bilal@server.com>
2012-12-18 15:21:20 -0500

    gallery viewer imgur api 3 fix

--- gallery-viewer.js
+++ gallery-viewer.js
@@ -2,18 +2,48 @@
 * 
 */
 
-var id = "UcUlZ";
+var id = "qOlBw";
 var albumUrl = "http://api.imgur.com/2/album/"+id+".json";
+var albumUrl2 =" https://api.imgur.com/3/album/"+id+"/images";
+var client_id="client_id: 577b889f51c4ccc";
 $(document).ready(function(){
+	//getJSON(albumUrl2);
+	getAjaxJSON(albumUrl2);
+});
+
+
+function getJSON(albumUrl){
 	$.getJSON(albumUrl,
 		function(data){
-			var images = data.album.images;
+			$("\#dynamic-gallery").append("test"+data.images);
+			/*var images = data.album.images;
 			$.each(images, function(i,item){
 				var div = $("<div></div>").attr("id","gallery-item");
 				$("<img/>").attr("src",item.links.large_thumbnail).appendTo(div);
 				div.appendTo("#dynamic-gallery");
-			});			
+			});	*/		
 		});
-});
+}
+
+function getAjaxJSON(albumUrl){
+	$.ajax({
+		type: "POST",
+    url: albumUrl,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    
+    beforeSend: function ( xhr ) {
+    xhr.setRequestHeader('Authorization', client_id);
+  },
+    
+    success: function(json) {
+        $("#dynamic-gallery").html("json.length=" + json.length);
+        
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        $("#dynamic-gallery").html(xhr.responseText);
+    }
+	});
+}
 
 
