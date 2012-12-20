102e44685bcb2b008172362db3fc926810cc8cad
Bilal Al <bilal@server.com>
2012-12-20 01:42:40 -0500

    finalized the script to now accept custom attributes for album id

--- gallery-viewer.js
+++ gallery-viewer.js
@@ -1,50 +1,43 @@
 /**
-* 
+* JQuery script for loading images from the facebook page album providing the album id as follows:
+* <div class="fb-album" data-id="{ALBUM_ID}"></div>
+* images will be added in the following format <div class="fb-image"><img src="{image_source}"/>
+* Author: Bilal Al-Hajjar
 */
 
-var id = "qOlBw";
-var albumUrl = "http://api.imgur.com/2/album/"+id+".json";
-var albumUrl2 =" https://api.imgur.com/3/album/"+id+"/images?callback=0";
-var auth="Client-ID 54a3f7332a8786f";
-var facebook= "https://graph.facebook.com/233975233402550/photos";
+var facebookUrl = "https://graph.facebook.com/";
+var photosExt = "/photos?fields=source";
 $.support.cors = true;
 $(document).ready(function(){
-	$("#dynamic-gallery").append("document ready");
-	//getJSON(albumUrl2);
-	getAjaxJSON(facebook);
+	var id =$('.fb-album').attr('data-id');
+	var albumUrl = facebookUrl + id + photosExt 
+	getAjaxJSON(albumUrl);
 
 });
-
-
-function getJSON(albumUrl){
-	$("#dynamic-gallery").append("getJson");
-	$.getJSON(albumUrl,
-		function(data){
-			$("\#dynamic-gallery").append("data " +data);
-			/*var images = data.album.images;
-			$.each(images, function(i,item){
-				var div = $("<div></div>").attr("id","gallery-item");
-				$("<img/>").attr("src",item.links.large_thumbnail).appendTo(div);
-				div.appendTo("#dynamic-gallery");
-			});	*/		
-		});
-}
 
 function getAjaxJSON(albumUrl){
 $.ajax({
     url: albumUrl,
     dataType: "json",
     type: "GET",
-    success: function(json) {
-        $("#dynamic-gallery").append("success "+json);
+    success: function(data) {
+        
+        var dataArray = data.data;
+        $.each(dataArray, function(i,item){
+        	console.log(item.source);
+        	var div = $("<div></div>").attr("class","fb-image");
+        	$("<img/>").attr("src",item.source).appendTo(div);
+				div.appendTo(".fb-album");
+        });
+        console.log(dataArray);
         
     },
     error: function (xhr, textStatus, errorThrown) {
-        $("#dynamic-gallery").append(" error " + textStatus);
+        console.log("error "+ textStatus);
         
     },
     complete: function(data){
-    	$("#dynamic-gallery").append(" complete "+data);
+    	console.log("complete");
     }
 	});
 
